ec4e2ca18c5c81ea02b78b98bd148f422bda122f
Osvald Frisk <osvald.frisk@gmail.com>
2019-11-02 23:29:02 +0100

    Continued work on parsing, atm showing the abstract syntax tree without evaluation.

---
+++ parsing.c
@@ -0,0 +1,49 @@
+#include "mpc.h"
+#include <editline/readline.h>
+
+int main(int argc, char** argv) {
+  
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Tyson = mpc_new("tyson");
+  
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      tyson    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Tyson);
+  
+  puts("Tyson Version 0.0.0.0.2");
+  puts("Press Ctrl+c to Exit\n");
+  
+  while (1) {
+  
+    char* input = readline("tyson> ");
+    add_history(input);
+    
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Tyson, &r)) {
+      /* On success print and delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
+    free(input);
+  }
+  
+  /* Undefine and delete our parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Tyson);
+  
+  return 0;
+}
